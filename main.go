ce18c18905693e646895e2b5c218545b15a44599
Jason Stillwell <jstillwell@wheels.co>
2019-10-05 00:08:19 -0700

    .

--- main.go
+++ main.go
@@ -283,6 +283,80 @@ func removePlacedEntity(input []*PlacedEntity, entity *PlacedEntity) []*PlacedEn
 	panic("can't remove entity")
 }
 
+type Subscription struct {
+	cancelled   bool
+	Channel     chan bool
+	EventSource *EventSource
+}
+
+func NewSubscription(e *EventSource) *Subscription {
+	return &Subscription{Channel: make(chan bool)}
+}
+
+func (s *Subscription) cancel() {
+	s.cancelled = true
+	for i, sub := range s.EventSource.Subscriptions {
+		if sub == s {
+			s.EventSource.Subscriptions = append(s.EventSource.Subscriptions[:i], s.EventSource.Subscriptions[i+1:]...)
+			break
+		}
+	}
+	s.Channel <- false
+}
+
+func (s *Subscription) wait() {
+}
+
+type EventSource struct {
+	Subscriptions []*Subscription
+}
+
+func NewEventSource() *EventSource {
+	return &EventSource{Subscriptions: make([]*Subscription, 0)}
+}
+
+func (e *EventSource) subscribe() *Subscription {
+	s := &Subscription{Channel: make(chan bool)}
+	e.Subscriptions = append(e.Subscriptions, s)
+	return s
+}
+
+var entityMovementEvent EventSource = EventSource{}
+
+// for regular entities these subscriptions might be in the Entity struct
+var weaponSwingSubscription *Subscription
+
+// cancel the swing before swinging again, or interrupted by something like picking up another weapon
+func cancelSwingWeapon() {
+
+	if weaponSwingSubscription != nil {
+		weaponSwingSubscription.cancel()
+	}
+	weaponSwingSubscription = nil
+}
+
+func swingWeapon() {
+
+	cancelSwingWeapon()
+
+	subscription := entityMovementEvent.subscribe()
+	weaponSwingSubscription = subscription
+	go func() {
+		defer cancelSwingWeapon()
+		for {
+			subscription.wait()
+			if subscription.cancelled {
+				break
+			}
+			if weaponRotationDone() {
+				break
+			}
+			rotateWeapon()
+		}
+		returnWeapon()
+	}()
+}
+
 func main() {
 
 	spriteMap := flag.Bool("sprite-map", false, "show the sprites")
@@ -412,6 +486,9 @@ func main() {
 					case sdl.K_SPACE:
 						// attack
 						attackTimer = 3
+						if weilded != nil {
+							swingWeapon()
+						}
 					}
 
 				}
