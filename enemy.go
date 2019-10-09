f743fe6157fce308bf8ee287e3da9191d50d0769
Jason Stillwell <jstillwell@wheels.co>
2019-10-08 23:53:39 -0700

    .

--- enemy.go
+++ enemy.go
@@ -32,25 +32,29 @@ func drawMonsterWillMove(r *sdl.Renderer) {
 	for _, monster := range monsters {
 		if willMove, ok := monster.Data.(WillMove); monster.Data != nil && ok {
 			// full image
-			arrowRect := sdl.Rect{0, 0, 16, 16}
+			arrowRect := sdl.Rect{X: 0, Y: 0, W: 16, H: 16}
 
 			// default angle is facing down
-			angle := 0.0
+			var angle float64
 			if willMove.X-monster.X > 0 {
-				angle = 180
+				angle = 0
 			} else if willMove.Y-monster.Y > 0 {
-				angle = 280
-			} else if monster.Y-willMove.Y > 0 {
 				angle = 90
+			} else if monster.Y-willMove.Y > 0 {
+				angle = 270
+			} else {
+				angle = 180
 			}
 
 			// draw half way between the 2 points.
 			tgtRect := sdl.Rect{
-				int32(willMove.X+monster.X) * PIXELS_PER_CELL / 2,
-				int32(willMove.Y+monster.Y) * PIXELS_PER_CELL / 2,
-				16,
-				16}
-			err := r.CopyEx(pixelTex, &arrowRect, &tgtRect, angle, nil, 0)
+				X: int32(willMove.X+monster.X) * PIXELS_PER_CELL / 2,
+				Y: int32(willMove.Y+monster.Y) * PIXELS_PER_CELL / 2,
+				W: 16,
+				H: 16,
+			}
+			fmt.Printf("drawing arrow at %v\n", tgtRect)
+			err := r.CopyEx(moveArrowTexture, &arrowRect, &tgtRect, angle, nil, 0)
 			if err != nil {
 				panic(err)
 			}
@@ -71,6 +75,15 @@ func monstersThink() {
 		if willMove.X < -1 {
 			willMove.X = -1
 		}
+		willMove.X = monster.X + willMove.X
+		willMove.Y = monster.Y
+
+		// keep it if there is nothing there or the player there.
+		if len(otherEntitiesAt(character, willMove.X, willMove.Y)) == 0 {
+			monster.Data = willMove
+			return
+		}
+
 		willMove.Y = character.Y - monster.Y
 		if willMove.Y > 1 {
 			willMove.Y = 1
@@ -78,15 +91,14 @@ func monstersThink() {
 		if willMove.Y < -1 {
 			willMove.Y = -1
 		}
-
-		willMove.X = monster.X + willMove.X
 		willMove.Y = monster.Y + willMove.Y
+		willMove.X = monster.X
 
 		// keep it if there is nothing there or the player there.
 		if len(otherEntitiesAt(character, willMove.X, willMove.Y)) == 0 {
-			fmt.Printf("monster will move to %v\n", willMove)
 			monster.Data = willMove
 		}
+
 	}
 }
 
@@ -112,7 +124,6 @@ func findEmptyPosition() (x, y int) {
 		for y := 0; y < MAX_Y; y++ {
 			occupied := entitiesAt(x, y)
 			if len(occupied) == 0 {
-				fmt.Printf("found empty cell at %d,%d\n", x, y)
 				return x, y
 			}
 		}
